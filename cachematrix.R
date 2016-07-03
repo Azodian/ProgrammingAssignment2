59b15c69980884e381a5fc5a7867ebd2031d1755
Brennen Peckinpaugh <Brennen.e.Peckinpaugh@gmail.com>
2016-07-03 10:17:08 -0500

    Finished Programming Assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This pair of functions creates and caches a matrix or matrices,
+## to be restored and inverted at a later time. It can store as many
+## matrices as needed.
 
-## Write a short comment describing this function
+## This is a variant on the makeVector function provided as an example
+## with the assignment.
+## The matrix must be square (same number of rows as columns), but
+## that's a requirement for it to be invertable at all and not a bug.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinv <- function(s) m <<- s
+  getinv <- function() m
+  list(set = set, 
+       get = get,
+       setinv = setinv,
+       getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## This inverts the matrix given, or gets the cached matrix and
+## inverts it.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getinv()
+  if(!is.null(m)) {
+    message("Rooting through cache")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinv(m)
+  m
 }
